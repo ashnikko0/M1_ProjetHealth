4fde472365987cd373650ba0198b2bc016b29a3e
Maxiss <maxime.colella@yahoo.com>
2024-04-05 16:47:33 +0200

    add icons + prépa link navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,11 @@ export default function Nav(){
         <div className="navbar">
            <ul className="nav-links">
               <Link to="/">TableauDeBord</Link>
+                  <img src={dashboard.png} alt= "TableauDeBord"/>
               <Link to="/login">Login</Link>
+                  <img src={log.png} alt= "Login"/>
               <Link to="/clients_list">Clients List</Link>
+                  <img src={stats.png} alt= "ListeClients"/>
            </ul>
         </div>
   );
