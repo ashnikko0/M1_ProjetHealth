cab47b14dc9ad235bb1e8de8825b3be9d25ec97f
Amandine <amandine.schang@hotmail.fr>
2024-04-05 18:00:01 +0200

    squelette clientCard

--- src/components/ClientCard.jsx
+++ src/components/ClientCard.jsx
@@ -3,13 +3,23 @@ import femaleIcon from "../assets/femaleIcon.svg";
 
 function ClientCard({ client }) {
 
+  //chaque div de la card aura un style particulier
   return (
     <div className="client-card">
+
+      <div>
       <h2>{client.firstname} {client.lastname}</h2>
+      </div>
+
+      <div>
+      {client.sex === 1 ? <img src={maleIcon} alt="maleIcon" className="sexIcon"/> : <img src={femaleIcon} alt="femaleIcon" className="sexIcon"/>}
       <p>Taille : {client.height}</p>
+      </div>
+
+      <div>
       <p>Poids de départ : {client.weightStart}</p>
       <p>Objectif poids : {client.weightGoal}</p>
-      {client.sex === 1 ? <img src={maleIcon} alt="maleIcon" className="sexIcon"/> : <img src={femaleIcon} alt="femaleIcon" className="sexIcon"/>}
+      </div>
       
     </div>
   );
@@ -19,4 +29,5 @@ export default ClientCard;
 
 /*faire un beau découpage pour avoir des belles cartes
 
--afficher un icone si l'objectif poids est atteint */
+-afficher un icone si l'objectif poids est atteint 
+-ajouter des photos de gens ? */
