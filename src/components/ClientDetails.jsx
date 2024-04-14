72212e37020fbd4f34eb2da2e1182cf66a68cc44
Questiero <19290903+Questiero@users.noreply.github.com>
2024-04-14 18:27:24 +0200

    Recharts premier jet

--- src/components/ClientDetails.jsx
+++ src/components/ClientDetails.jsx
@@ -1,12 +1,46 @@
+import { useState, useEffect } from 'react';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, ReferenceLine, ResponsiveContainer } from 'recharts';
+import axios from 'axios';
 
 function ClientDetails() {
 
   const { id } = useParams(); // Récupérer l'ID du client depuis l'URL
+  const [physioData, setPhysioData] = useState(null); // État pour stocker les détails du client
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
 
   const client = location.state.client;
+
+  useEffect(() => {
+    async function loadPhysioData() {
+
+      const api = "https://health.shrp.dev/items/physiologicalData?filter[people_id][_eq]=" + id;
+
+      try {
+
+        setIsLoading(true);
+        setIsError(false);
+
+        const response = await axios.get(api);
+
+        const data = await response.data.data;
+
+        setPhysioData(data);
+        setIsLoading(false);
+        setIsError(false);
+
+      } catch (error) {
+        setIsError(true);
+        setIsLoading(false);
+      }
+    }
+
+    loadPhysioData();
+
+  }, []);
 
   // Si les détails du client sont en cours de chargement, afficher un indicateur de chargement
   return (
@@ -18,7 +52,19 @@ function ClientDetails() {
         <p>Taille: {client.height}</p>
         <p>Poids de départ: {client.weightStart}</p>
         <p>Objectif poids: {client.weightGoal}</p>
-      </div>   
+      </div>
+      {isLoading && <div className="loader"/>}
+      {isError && <p>Une erreur s'est produite</p>}
+      {physioData && 
+        <ResponsiveContainer width="80%" height={200}>
+          <LineChart data={physioData}>
+            <XAxis dataKey="date"/>
+            <YAxis domain={['dataMin - 1', 'dataMax + 1']}/>
+            <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+            <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+            <ReferenceLine y={client.weightGoal} label="Goal" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>}
     </div>
   );
 }
