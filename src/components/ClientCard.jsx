67d327f6f78951642acad336d329e31790ddb24f
Amandine <amandine.schang@hotmail.fr>
2024-04-16 15:06:13 +0200

    tout propre

--- src/components/ClientCard.jsx
+++ src/components/ClientCard.jsx
@@ -14,48 +14,33 @@ function ClientCard({ client }) {
   return (
     <div className="client-card" onClick={() => navigate('/client/' + client.id, {state:{client}})}>
         
-        <div>
-        <h2>{client.firstname} {client.lastname}</h2>
+        <div className="name-card">
+        {client.firstname} {client.lastname}
         </div>
 
-<<<<<<< HEAD
-        <div className="mid-part-card">
-        <img src={ruler} alt="Ruler" className="icon" />
-        <p>Taille : {client.height}</p>
-        {client.sex === 1 ? <img src={maleIcon} alt="maleIcon" className="icon"/> : <img src={femaleIcon} alt="femaleIcon" className="icon"/>}
-        </div>
-        
-        <div className="bottom-part-card">
-        <p>{client.weightStart}</p>
-        <img src={current} alt="CurrentWeight" className="icon" />
-        <img src={goal} alt="GoalWeight" className="icon" />
-        <p> {client.weightGoal}</p> 
-        {client.weightStart === client.weightGoal ? <img src={trophy} alt="trophy" className="icon"></img> :<span></span>}
-=======
         <div className="content-card">
 
           <div className="inside-card">
-            <img src={ruler} alt="Ruler" className="sexIcon" />
+            <img src={ruler} alt="Ruler" className="icon" />
             <p>{client.height}</p>
           </div>
         
-        {client.sex === 1 ? <img src={maleIcon} alt="maleIcon" className="sexIcon"/> : <img src={femaleIcon} alt="femaleIcon" className="sexIcon"/>}
+        {client.sex === 1 ? <img src={maleIcon} alt="maleIcon" className="icon"/> : <img src={femaleIcon} alt="femaleIcon" className="icon"/>}
         </div>
         
         <div className="content-card">
           <div className="inside-card">
-          <img src={current} alt="CurrentWeight" className="sexIcon" />
+          <img src={current} alt="CurrentWeight" className="icon" />
           <p>{client.weightStart}</p>
         
         </div>
 
         <div className="inside-card">
-          <img src={goal} alt="GoalWeight" className="sexIcon" />
+          <img src={goal} alt="GoalWeight" className="icon  " />
           <p> {client.weightGoal}</p> 
-          {client.weightStart === client.weightGoal ? <img src={trophy} alt="trophy" className="sexIcon"></img> :<span></span>}
+          {client.weightStart === client.weightGoal ? <img src={trophy} alt="trophy" className="icon"></img> :<span></span>}
         </div>
 
->>>>>>> 84263b18a2d291e1ac9993828b300eae5a297db7
         </div>
       
     </div>
